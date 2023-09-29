7e72ba1568eebbe267bbae44ec56229d69ac52ad
ferty460 <rostovcevvladimir460@gmail.com>
2023-09-29 12:39:42 +0300

    Правильное отображение tooltip

--- src/main/java/com/example/calendar/util/CalendarUtil.java
+++ src/main/java/com/example/calendar/util/CalendarUtil.java
@@ -2,6 +2,7 @@ package com.example.calendar.util;
 
 import com.example.calendar.model.Event;
 import com.example.calendar.model.date.Day;
+import com.example.calendar.model.date.Month;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -9,10 +10,11 @@ import java.util.List;
 @Component
 public class CalendarUtil {
 
-    public String checkEventsInDay(List<Event> events, Day day) {
+    public String checkEventsInDay(List<Event> events, Day day, Month month) {
         StringBuilder result = new StringBuilder();
         for (Event event : events) {
-            if (event.getDate().toString().substring(8, 10).replaceAll("^0+","").equals("" + day.getDayOfMonth())) {
+            if (event.getDate().toString().substring(8, 10).replaceAll("^0+","").equals("" + day.getDayOfMonth())
+                    && event.getDate().toString().substring(5, 7).replaceAll("^0+","").equals("" + month.getMonth())) {
                 result.append("\n");
                 result.append(event.getName());
             }
